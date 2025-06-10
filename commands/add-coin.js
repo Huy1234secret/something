9f251335b0b6e8ba2a2148df2f42c9eac4644835
Huy1234secret <9a8phamminhhuy@gmail.com>
2025-06-10 17:19:22 +0700

    Create add-coin.js

---
+++ commands/add-coin.js
@@ -0,0 +1,23 @@
+// commands/add-coin.js
+const { SlashCommandBuilder, PermissionsBitField, ApplicationCommandOptionType } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('add-coin')
+        .setDescription('Add coins to a user (Staff Only).')
+        .addUserOption(option =>
+            option.setName('user')
+                .setDescription('The user to add coins to.')
+                .setRequired(true))
+        .addIntegerOption(option =>
+            option.setName('amount')
+                .setDescription('Amount of coins to add (negative to remove).')
+                .setRequired(true))
+        .setDefaultMemberPermissions(PermissionsBitField.Flags.ManageGuild),
+    async execute(interaction) {
+        // This command's logic is primarily handled in index.js for centralizing user stat modifications.
+        // It's defined here for command deployment.
+        await interaction.reply({ content: 'Processing add-coin command...', ephemeral: true });
+        // The actual logic is in index.js under interactionCreate listener.
+    },
+};
