833b143ce14fa4675df98e0a66213a6c46a79c80
huypham <9a8phamminhhuy@gmail.com>
2025-08-05 01:23:13 +0700

    feat: alert when build battle submissions close

--- deployCommands.js
+++ deployCommands.js
@@ -619,10 +619,6 @@ const commands = [
         ]
     },
     {
-        name: 'submit-ticket',
-        description: 'Create a private build submission channel.'
-    },
-    {
         name: 'reroll-theme',
         description: 'Reroll a build battle theme for a user.',
         options: [
