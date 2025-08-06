7223d36f336a9ce2b2ed61cecaa7ca559d666f40
huypham <9a8phamminhhuy@gmail.com>
2025-08-06 23:43:43 +0700

    feat: expand admin weather and event commands

--- commands/start-weather.js
+++ commands/start-weather.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder, PermissionsBitField } = require('discord.js');
-const { startRain } = require('../utils/weatherManager');
+const { startRain, startGoldenRain, startSnowRain, startSolarFlare } = require('../utils/weatherManager');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -10,24 +10,50 @@ module.exports = {
                 .setDescription('Weather ID')
                 .setRequired(true)
                 .addChoices(
-                    { name: 'Rain', value: 'rain' }
+                    { name: 'Rain', value: 'rain' },
+                    { name: 'Golden Rain', value: 'goldenRain' },
+                    { name: 'Snow Rain', value: 'snowRain' },
+                    { name: 'Solar Flare Surge', value: 'solarFlare' }
                 )
         )
         .setDefaultMemberPermissions(PermissionsBitField.Flags.ManageGuild),
     async execute(interaction) {
         const id = interaction.options.getString('id');
         let response;
+        let result;
         if (id === 'rain') {
-            const result = await startRain(interaction.client, { byAdmin: true });
-            if (!result.started) {
-                const endTs = Math.floor((Date.now() + result.remaining) / 1000);
-                response = { content: `The weather Rain is already active, you can activate again in <t:${endTs}:R>`, ephemeral: true };
-            } else {
-                response = { content: 'Rain has started.', ephemeral: true };
-            }
+            result = await startRain(interaction.client, { byAdmin: true });
+        } else if (id === 'goldenRain') {
+            result = await startGoldenRain(interaction.client, { byAdmin: true });
+        } else if (id === 'snowRain') {
+            result = await startSnowRain(interaction.client, { byAdmin: true });
+        } else if (id === 'solarFlare') {
+            result = await startSolarFlare(interaction.client, { byAdmin: true });
         } else {
             response = { content: 'Unknown weather ID.', ephemeral: true };
         }
+
+        if (!response) {
+            if (!result.started) {
+                const endTs = Math.floor((Date.now() + result.remaining) / 1000);
+                const nameMap = {
+                    rain: 'Rain',
+                    goldenRain: 'Golden Rain',
+                    snowRain: 'Snow Rain',
+                    solarFlare: 'Solar Flare Surge'
+                };
+                response = { content: `The weather ${nameMap[id]} is already active, you can activate again in <t:${endTs}:R>`, ephemeral: true };
+            } else {
+                const nameMap = {
+                    rain: 'Rain',
+                    goldenRain: 'Golden Rain',
+                    snowRain: 'Snow Rain',
+                    solarFlare: 'Solar Flare Surge'
+                };
+                response = { content: `${nameMap[id]} has started.`, ephemeral: true };
+            }
+        }
+
         if (interaction.deferred || interaction.replied) return interaction.editReply(response);
         return interaction.reply(response);
     },
