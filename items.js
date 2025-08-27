fea4b4aa959dacc6246d91ccb02581bf14d409e5
huypham <9a8phamminhhuy@gmail.com>
2025-08-27 20:45:52 +0700

    Fix emoji format for TotemOfUndying

--- items.js
+++ items.js
@@ -15,7 +15,7 @@ const ITEMS = {
   TotemOfUndying: {
     id: 'TotemOfUndying',
     name: 'Seraphic Heart',
-    emoji: '<a:ITSeraphicheart:1410243519372853401>',
+    emoji: '<ITSeraphicheart:1410243519372853401>',
     rarity: 'Rare',
     value: 2000,
     useable: false,
